e72f367dcfd3487af9ad449625427a9cb6e760bf
Piotr Bartnik <piotr.bartnik@hotmail.com>
2022-10-31 08:27:28 +0100

    chore: add actions to inc, dec buttons

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,8 +1,26 @@
-<script setup lang="ts"></script>
-
 <template>
-  <template>
-    <button>-</button>
-    <button>+</button>
-  </template>
+  <button @click="decrement">-</button>
+  <button @click="increment">+</button>
+  <span>{{ value }} </span>
 </template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      msg: "Counter App",
+      value: 0,
+    };
+  },
+  methods: {
+    decrement() {
+      if (this.value) {
+        this.value -= 1;
+      }
+    },
+    increment() {
+      this.value += 1;
+    },
+  },
+};
+</script>
